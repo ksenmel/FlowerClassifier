0b8c933420056fa49ea58238a224d1ca4248b286
ksenmel <ksemelashenko@gmail.com>
2025-01-25 02:21:03 +0300

    major accuracy fix

--- classification/classifier.py
+++ classification/classifier.py
@@ -5,44 +5,28 @@ import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score
 
 
 class Classifier:
     def __init__(self):
         self.model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-    def fit(self, df: pd.DataFrame):
+    def fit(self, x_train: pd.DataFrame, y_train: pd.DataFrame):
         """
         Обучает модель на данных.
-
-        Args:
-            X (np.ndarray): Признаки (например, извлеченные признаки изображений).
-            y (np.ndarray): Метки классов (например, номера типов цветов).
-            :param df:
         """
-        X = df.drop(columns=['label']).values
-        y = df['label'].values
+        self.model.fit(x_train, y_train)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
-
-        self.model.fit(X_train, y_train)
-
-    def predict(self, df: pd.DataFrame) -> np.ndarray:
+    def predict(self, x_test: pd.DataFrame) -> pd.DataFrame:
         """
         Предсказывает классы для новых данных.
         """
-        X = df.drop(columns=['label']).values
-        predictions = self.model.predict(X)
+        predictions = self.model.predict(x_test)
         return predictions
 
-    def evaluate(self, df: pd.DataFrame) -> float:
-        X = df.drop(columns=['label']).values
-        y = df['label'].values
+    def evaluate(self, y_test: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
-
-        y_pred = self.model.predict(X_test)
         return accuracy_score(y_test, y_pred)
 
     def download(self, directory: str, model_name: str = 'v1FlowerClassifier_model'):
@@ -69,7 +53,7 @@ class Classifier:
         self.model = joblib.load(model_path)
         print(f"Модель загружена из {model_path}")
 
-    def predict_proba(self, df: pd.DataFrame) -> np.ndarray:
+    def predict_proba(self, x: pd.DataFrame) -> np.ndarray:
         """
         Выводит распределение вероятностей для каждого класса.
 
@@ -79,7 +63,6 @@ class Classifier:
         Returns:
             np.ndarray: Массив вероятностей для каждого класса для каждого примера.
         """
-        X = df.drop(columns=['label']).values
-        proba = self.model.predict_proba(X)
+        proba = self.model.predict_proba(x)
         return proba
 
