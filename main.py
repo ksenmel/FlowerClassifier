a4bca3efb492a0d5bfcfe8791ad3095172757c2a
ksenmel <ksemelashenko@gmail.com>
2025-01-25 05:57:03 +0300

    add more info about distribution

--- main.py
+++ main.py
@@ -12,14 +12,18 @@ from classification.features import feature_functions
 def train_mode(args):
     dataset = FlowerDataset(path=args.dataset_path)
 
-    print(f"Number of samples in the dataset: {len(dataset)} \n There are {len(dataset.classes)} classes: {dataset.classes} \n")
+    print(
+        f"Number of samples in the dataset: {len(dataset)} \n There are {len(dataset.classes)} classes: {dataset.classes} \n"
+    )
 
     df = dataset.create_df()
 
-    X = df.drop(columns=['label']).values
-    y = df['label'].values
+    X = df.drop(columns=["label"]).values
+    y = df["label"].values
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42, shuffle=True
+    )
 
     classifier = Classifier()
 
@@ -45,25 +49,44 @@ def predict_mode(args):
 
     X = df.values
 
+    class_names = ["daisy", "dandelion", "rose", "sunflower", "tulip"]
+
     predictions = classifier.predict(X)
-    print("Predictions:", predictions)
+    print(f"I think the flower on the image is {class_names[predictions[0]]}!")
 
     probs = classifier.predict_proba(X)
 
-    print("Classes probability distribution for image:")
-    print(probs)
+    print(
+        f"Classes probability distribution for image: \n{probs[0][0]} daisy\n{probs[0][1]} dandelion\n{probs[0][2]} rose\n{probs[0][3]} sunflower\n{probs[0][4]} tulip\n"
+    )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parse parameters for the flower classification application")
-    parser.add_argument("--mode", help="Mode of the application: 'train' for training, 'predict' for evaluating",
-                        choices=["train", "predict"], default="predict")
-    parser.add_argument("-i", "--img_path", help="Path to the input image with flower to classify",
-                        default="tests/0.jpeg")
+    parser = argparse.ArgumentParser(
+        description="Parse parameters for the flower classification application"
+    )
+    parser.add_argument(
+        "--mode",
+        help="Mode of the application: 'train' for training, 'predict' for evaluating",
+        choices=["train", "predict"],
+        default="predict",
+    )
+    parser.add_argument(
+        "-i",
+        "--img_path",
+        help="Path to the input image with flower to classify",
+        default="tests/0.jpeg",
+    )
     parser.add_argument("-m", "--model_path", help="Path to classification model")
-    parser.add_argument("-d", "--dataset_path", help="Path to the dataset for training",
-                        default="models/v1FlowerClassifier_model.pkl")
-    parser.add_argument("-s", "--path_to_save", help="path to save classification model")
+    parser.add_argument(
+        "-d",
+        "--dataset_path",
+        help="Path to the dataset for training",
+        default="models/v1FlowerClassifier_model.pkl",
+    )
+    parser.add_argument(
+        "-s", "--path_to_save", help="path to save classification model"
+    )
 
     args = parser.parse_args()
 
